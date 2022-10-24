fcd1f7806f252409795261bfe5dcf49645114396
reuby-dev <delphineb57@gmail.com>
2022-10-24 12:47:21 +0200

    add animation card

--- js/index.js
+++ js/index.js
@@ -1,6 +1,12 @@
 class TextReplication {
     constructor(input) {
         this.input = input
+
+        //animation
+        
+        this.input.addEventListener('focus', this.animate.bind(this))
+
+        //replicate text
         let elementFocusedId = this.input.getAttribute('data-text-replicate')
         if (elementFocusedId === null) {
             console.warn("no attribute data-text-replicate on input")
@@ -17,9 +23,29 @@ class TextReplication {
     replicate(e) {
         this.target.innerText = e.target.value
     }
+
+    animate(e) {
+        let elementFocusedPosition = this.input.getAttribute('data-card-position')
+        let frontCard = document.getElementsByClassName('front-card')[0]
+        let isElementBackPosition = frontCard.classList.contains('go-back')
+        let backCard = document.getElementsByClassName('back-card')[0]
+        console.log(isElementBackPosition)
+        if(elementFocusedPosition === 'front' && isElementBackPosition) {
+            frontCard.classList.remove('go-back')
+            frontCard.classList.add('go-front')
+            backCard.classList.remove('go-front')
+            backCard.classList.add('go-back')
+        }
+        else if(elementFocusedPosition === "back") {
+            backCard.classList.remove('go-back')
+            backCard.classList.add('go-front')
+            frontCard.classList.remove('go-front')
+            frontCard.classList.add('go-back')
+        }
+    }
 }
 
-let inputs = document.querySelectorAll('input')
+let inputs = document.querySelectorAll('input[type=text]')
 inputs.forEach(input => {
     new TextReplication(input)
 })
