cd3ad105163cc20c838399b44cd441c75a3a96fe
reuby-dev <delphineb57@gmail.com>
2022-10-24 10:42:08 +0200

    add js comportment for replicate text

--- js/index.js
+++ js/index.js
@@ -1,1 +1,25 @@
-console.log('coucou')
+class TextReplication {
+    constructor(input) {
+        this.input = input
+        let elementFocusedId = this.input.getAttribute('data-text-replicate')
+        if (elementFocusedId === null) {
+            console.warn("no attribute data-text-replicate on input")
+            return
+        }
+        this.target = document.getElementById(elementFocusedId)
+        if (this.target === null) {
+            console.warn("target not found, researched id is :", elementFocusedId)
+            return
+        }
+        this.input.addEventListener('keyup', this.replicate.bind(this))
+    }
+
+    replicate(e) {
+        this.target.innerText = e.target.value
+    }
+}
+
+let inputs = document.querySelectorAll('input')
+inputs.forEach(input => {
+    new TextReplication(input)
+})
